14b57a1a7af67bb9a919438855280da96f9848f4
Angela Lo <angelawenlo@gmail.com>
2019-05-21 13:59:21 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,10 +4,8 @@ def reformat_languages(languages)
   new_hash = {languages.merge}
   languages.each do |programming, data|
     data.each do |language, type_info|
-    if programming.keys == :oo || languages.keys.include?(:functional)
 
     binding.pry
-      end
     end
   end
 end
