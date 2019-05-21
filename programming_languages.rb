35661d1de001f862176689f75801f22c44195b84
Angela Lo <angelawenlo@gmail.com>
2019-05-21 14:48:10 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,12 @@
 require 'pry'
 def reformat_languages(languages)
   # your code her
-  new_hash = {}
+  functional_hash = {}
   #new_hash = languages.values
   languages.values.each do |programming, data|
     data.each do |language, type_info|
       if programming == :functional
-        new_hash[:style] = ["functional"]
+
         puts new_hash
     binding.pry
         end
