da0acce2b5f76fb7a06d8d958d8fddaae5a87a13
Angela Lo <angelawenlo@gmail.com>
2019-05-21 13:27:00 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,8 @@ require 'pry'
 def reformat_languages(languages)
   # your code here
   languages.each do |programming, language|
-    language.each do
+    language.each do |type, type_info|
+
     binding.pry
   end
 end
