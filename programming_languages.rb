54d90882265fde0356045a67c3c8b5398e3f0bb0
Angela Lo <angelawenlo@gmail.com>
2019-05-21 14:36:58 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,7 +6,7 @@ def reformat_languages(languages)
   languages.each do |programming, data|
     data.each do |language, type_info|
       if programming == :functional
-        data << [:style] = [:functional]
+        new_hash[:style] = [:functional]
 
       elsif programming == :oo
         data = [:functional]
