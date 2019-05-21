04977576db7d336e233ae869b63c611d53ca94a6
Angela Lo <angelawenlo@gmail.com>
2019-05-21 15:44:12 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,6 @@ def reformat_languages(languages)
   # your code her
   new_hash = {}
   languages.each do |programming, data|
-    new_hash = data
     data.each do |language, info|
     if programming == :oo
 
