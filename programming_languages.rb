d3fa1a5602f65e2c326e7330e681bfb6c3ab8c28
Angela Lo <angelawenlo@gmail.com>
2019-05-21 15:40:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reformat_languages(languages)
   # your code her
   new_hash = {}
-  languages.map { |e|  } do |programming, data|
+  languages.map do |programming, data|
     new_hash = data
     data.map do |language, info|
     if programming == :oo
